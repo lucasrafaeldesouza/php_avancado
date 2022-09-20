da91d3b60db35aeecbf4e2ea9c0c459dc1091fe5
Lucas Rafael <56982485+lucasrafaeldesouza@users.noreply.github.com>
2022-09-20 15:24:55 -0300

    Voltando ao normal

--- index.php
+++ index.php
@@ -9,18 +9,3 @@ $vagas = Vaga::getVagas();
 include __DIR__.'/includes/header.php';
 include __DIR__.'/includes/listagem.php';
 include __DIR__.'/includes/footer.php';
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
